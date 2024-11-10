edd2eb696cb71aa54593dfbed46cbd3eb3cc42ff
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2024-11-10 16:05:31 +0000

    remove: Old MySQL file

--- database/script.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE ServerHealths (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  cpuUsage FLOAT NOT NULL,
-  memoryUsage FLOAT NOT NULL,
-  romUsage FLOAT NOT NULL,
-  bandwidth VARCHAR(255) NOT NULL
-);
