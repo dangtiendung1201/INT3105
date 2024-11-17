eaaa4d3bd6b7b214824557ea884f111cb68aeeb0
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2024-11-17 11:01:30 +0000

    chore: Change dir

--- routes/apiRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-import { checkApi } from '../controllers/apiController.js';
-
-router.post('/check', checkApi);
-
-export default router;
--- routes/containerRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-import { checkContainer } from '../controllers/containerController.js';
-
-router.post('/check', checkContainer);
-
-export default router;
