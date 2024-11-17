d651516ebc6f7fc97fe0bc7372acb29ef898d3d1
dangtiendung1201 <dangtiendung1201@gmail.com>
2024-11-18 05:09:42 +0700

    feat: Change method to count visit

--- forex-price-service/app.js
+++ forex-price-service/app.js
@@ -14,11 +14,8 @@ const SOCKET_URL = process.env.SOCKET_URL || 'http://localhost:3000';
 console.log('SOCKET_URL:', SOCKET_URL);
 const socket = io(SOCKET_URL);
 
-let trafficCounter = 0;
-
 app.use(morgan('combined'), (req, res, next) => {
-    trafficCounter += 1;
-    console.log(`Request count: ${trafficCounter}`);
+    socket.emit(NAMETAG, '1');
     next();
 });
 
@@ -40,17 +37,4 @@ socket.on('connect', () => {
   
   socket.on('disconnect', () => {
     console.log('Server disconnected');
-  });
-  
-  socket.on('checkTraffic', async (msg) => {
-    // Check body of the message: If content different from NAMETAG then ignore
-    if (msg !== NAMETAG) {
-      return;
-    }
-    console.log('message from server: ' + msg);
-    const result = await checkTraffic({ json: () => {}, status: () => ({ json: () => {} }) });
-    console.log(result);
-    
-    // Send message to socket server
-    socket.emit('receiveCounter', result);
   });
