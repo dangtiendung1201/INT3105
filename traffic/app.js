0877410332b954a800eaf13096929bf55c8b86ae
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2024-11-17 22:14:41 +0000

    feat: Improve ways to received and process msocket

--- traffic/app.js
+++ traffic/app.js
@@ -18,14 +18,11 @@ const io = new Server(server, {
 const HOST = '0.0.0.0';
 const PORT = process.env.PORT || 3004;
 
-const interval = process.env.INTERVAL || 5000;
-
 const TRAFFIC_NAMES = process.env.TRAFFIC_NAME.split(',');
 const TRAFFIC_ENDPOINTS = process.env.TRAFFIC_ENDPOINT.split(',');
 const TRAFFIC_NAMETAG = TRAFFIC_NAMES.map((name, index) => name + ':' + TRAFFIC_ENDPOINTS[index]);
 
 console.log('PORT:', PORT);
-console.log('Interval:', interval);
 console.log('TRAFFIC_NAMETAG:', TRAFFIC_NAMETAG);
 
 // Middleware
@@ -34,19 +31,20 @@ app.use(json());
 // Socket.io setup
 io.on('connection', (socket) => {
   console.log('New client connected');
-  setInterval(() => {
-    for (let i = 0; i < TRAFFIC_NAMETAG.length; i++) {
-      socket.emit('checkTraffic', TRAFFIC_NAMETAG[i]);
-    }
-  }
-    , interval);
-  socket.on('receiveCounter', async (msg) => {
-    // Print all contents of the message
-    console.log(msg);
 
-    // Write data to InfluxDB
-    await Traffic.writeData(msg.nameTag, msg.counter);
+  // Listen on the 'traffic' event named in TRAFFIC_NAMETAG
+  TRAFFIC_NAMETAG.forEach((nameTag) => {
+    socket.on(nameTag, (msg) => {
+      // Print all contents of the message
+      console.log(msg);
+
+      // Write data to InfluxDB concurrently
+      Traffic.writeData(msg.nameTag, 1).catch((err) => {
+        console.error(`Error writing data for ${nameTag}:`, err);
+      });
+    });
   });
+
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
