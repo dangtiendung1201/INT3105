6ba2a3531dcdf4ae49303a0bad09a0d74e9da19c
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2024-11-17 11:58:58 +0000

    chore: Put request in axiosInstance

--- server-health/config/db.js
+++ server-health/config/db.js
@@ -32,12 +32,9 @@ async function sendToInfluxDB(measurement, tags, fields) {
     // const body = 'server_health,bar_label=abc,source=grafana_cloud_docs test1=35.2,test2=35.2,test3=35.2,test4=35.2';
 
     try {
-        const response = await axios.post(GRAFANA_INFLUXDB_URL_WRITE, body, {
-            headers: {
-                Authorization: `Bearer ${GRAFANA_USERNAME}:${GRAFANA_API_TOKEN}`,
-                'Content-Type': 'text/plain',
-            },
-            httpAgent: new Agent({ keepAlive: true }),
+        const response = await axiosInstance({
+            method: 'post',
+            data: body
         });
 
         // Print the response
